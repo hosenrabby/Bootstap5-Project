16ae3efaa8eef856e41302301df5fda662164e18
Tahmid Tuhin <tahmid.tuhin.3@gmail.com>
2022-03-21 18:03:45 +0600

    Bootstrap5 Project

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//nav stycky 
+document.addEventListener("DOMContentLoaded", function () {
+
+    window.addEventListener('scroll', function () {
+
+        if (window.scrollY > 200) {
+            document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+        } else {
+            document.getElementById('navbar_top').classList.remove('fixed-top');
+            // remove padding top from body
+            document.body.style.paddingTop = '0';
+        }
+    });
+});
+
+// Owl carousel
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
